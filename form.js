5e7a9830f213a353798e6d2283870586b940581d
iammonali <monalisasamal1653@gmail.com>
2024-02-14 22:57:46 +0530

    first commit

---
+++ form.js
@@ -0,0 +1,6 @@
+let submitbtn = document.querySelector('button');
+submitbtn.addEventListener('click', showMsg);
+
+function showMsg(){
+    alert("Your form has been submitted succesfully. Thank You!!!");
+}
